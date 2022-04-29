e8a6a3f7dbe90a5ac6d7203877c5cd1a7ab1e3e2
Aqua <45574351+AquaPlaysYT@users.noreply.github.com>
2022-04-29 23:13:36 +0100

    feat: better discord rich presence

--- utils/discord.js
+++ utils/discord.js
@@ -5,6 +5,34 @@ const configHelper = require('./configHelper')
 const clientId = "948363491100721242";
 const client = new RPC.Client({ transport: 'ipc' });
 
+const rankIdToName = {
+    0: "Unranked",
+    1: "Unused I",
+    2: "Unused II",
+    3: "Iron I",
+    4: "Iron II",
+    5: "Iron III",
+    6: "Bronze I",
+    7: "Bronze II",
+    8: "Bronze III",
+    9: "Silver I",
+    10: "Silver II",
+    11: "Silver III",
+    12: "Gold I",
+    13: "Gold II",
+    14: "Gold III",
+    15: "Platium I",
+    16: "Platium II",
+    17: "Platinum III",
+    18: "Diamond I",
+    19: "Diamond II",
+    20: "Diamond III",
+    21: "Immortal I",
+    22: "Immortal II",
+    23: "Immortal III",
+    24: "Radiant",
+}
+
 client.on('ready', () => {
     const config = configHelper.getConfig();
     logger.discord(`Authed as ${client.user.username}#${client.user.discriminator}`);
@@ -13,18 +41,17 @@ client.on('ready', () => {
         client.request('SET_ACTIVITY', {
             pid: process.pid,
             activity : {
-                details : "Using Vulx",
-                state: "Launching",
+                details : "Valorant Profile Editor",
                 assets : {
                     large_image : "logo",
-                    large_text : "ValorantRC"
+                    large_text : "Vulx",
                 },
                 buttons : [{label : "Discord" , url : "https://discord.com/aquaplays"},{label : "Website" , url : "https://aquaplays.xyz"}]
             }
         })
 })
 
-module.exports.update = function(status) {
+module.exports.update = function(status, competitiveTier) {
     const config = configHelper.getConfig();
     //add rank support
     //add small image rank status [ small_image: " ]
@@ -32,11 +59,13 @@ module.exports.update = function(status) {
         client.request('SET_ACTIVITY', {
             pid: process.pid,
             activity : {
-                details : "Using Vulx",
-                state : status,
+                details : "Valorant Profile Editor",
+                state : `${status.length < 128 ? status : 'Playing Valorant'}`,
                 assets : {
                     large_image : "logo",
-                    large_text : "ValorantRC"
+                    large_text : "Vulx",
+                    small_image: `${competitiveTier}`,
+                    small_text: `${rankIdToName[competitiveTier]}`
                 },
                 buttons : [{label : "Discord" , url : "https://discord.com/aquaplays"},{label : "Website" , url : "https://aquaplays.xyz"}]
             }
