97c210b5fe64013219d074f8f3cee0469ea3a3dc
Aqua <45574351+AquaPlaysYT@users.noreply.github.com>
2022-07-30 03:40:57 +0100

    feat: dashboard ui overhaul
    Co-Authored-By: Syfe <22247810+ItsSyfe@users.noreply.github.com>

--- public/js/vulx.request.friends.js
+++ public/js/vulx.request.friends.js
@@ -7,6 +7,8 @@ function getDifference(array1, array2) {
   }
 
 function getTitleText(title) {
+    if (!title) return 'Friend';
+
     return fetch(`https://valorant-api.com/v1/playertitles/${title}`)
         .then(res => res.json())
         .then(res => res.data.titleText);
@@ -18,7 +20,7 @@ function getSelfPuuid() {
         .then(res => res.session.puuid)
 }
 
-fetch("http://127.0.0.1:/friends")
+fetch("http://127.0.0.1:/friends") //Add PUUID to div to allow for on click to locate you to their profile.
     .then(res => res.json())
     .then(async res => {
         var selfPuuid = await getSelfPuuid()
@@ -31,24 +33,23 @@ fetch("http://127.0.0.1:/friends")
         offlineFriends = getDifference(offlineFriends, onlineFriends);
         offlineFriends = offlineFriends.sort((a, b) => a.game_name.localeCompare(b.game_name));
 
-        for (var i = 0; i < onlineFriends.length / 3; i++) {
-            var friendCardsDiv = document.createElement("div");
-            friendCardsDiv.className = "search-bar-results-cards";
-            document.getElementsByClassName("search-bar-results")[0].appendChild(friendCardsDiv);
-
-            for (var j = 0; j < 3; j++) {
-                if (!onlineFriends[i * 3 + j]) break;
-
+        for (var j = 0; j < onlineFriends.length; j++) {
+            var searchBarResults = document.getElementsByClassName("search-bar-results")[0]
                 var friendCard = document.createElement("div");
                 friendCard.className = "search-bar-results-card";
-                friendCardsDiv.appendChild(friendCard);
+                searchBarResults.appendChild(friendCard);
 
-                var friendPrivate = await JSON.parse(atob(onlineFriends[i * 3 + j].private));
+                var friendPrivate = JSON.parse(atob(onlineFriends[j].private));
 
                 var friendCardImg = document.createElement("img"); 
                 friendCardImg.className = "searchPfp";
                 friendCardImg.src = `https://media.valorant-api.com/playercards/${friendPrivate.playerCardId}/smallart.png`;
                 friendCard.appendChild(friendCardImg);
+
+                var friendCardActivity = document.createElement("img"); 
+                friendCardActivity.className = "statusIcon";
+                friendCardActivity.src = `https://cdn.discordapp.com/attachments/808062997180317767/1002731345287581766/online.png`;
+                friendCard.appendChild(friendCardActivity);
 
                 var friendBannerContainer = document.createElement("div");
                 friendBannerContainer.className = "searchBannerCont";
@@ -61,11 +62,13 @@ fetch("http://127.0.0.1:/friends")
 
                 var friendSearchInfo = document.createElement("div");
                 friendSearchInfo.className = "searchInfo";
+                friendSearchInfo.id = "friend-search-info";
                 friendCard.appendChild(friendSearchInfo);
 
                 var friendNameText = document.createElement("h1");
                 friendNameText.className = "themeName-large5 textOverflow";
-                friendNameText.textContent = onlineFriends[i * 3 + j].game_name;
+                friendNameText.id = "friend-name";
+                friendNameText.textContent = onlineFriends[j].game_name;
                 friendSearchInfo.appendChild(friendNameText);
 
                 var friendTitleText = document.createElement("h3");
@@ -73,36 +76,21 @@ fetch("http://127.0.0.1:/friends")
                 friendTitleText.textContent = await getTitleText(friendPrivate.playerTitleId);
                 friendSearchInfo.appendChild(friendTitleText);
             }
-        }
-
-        var searchBarResultsTitle = document.createElement("div");
-        searchBarResultsTitle.className = "search-bar-results-title";
-        document.getElementsByClassName("search-bar-results")[0].appendChild(searchBarResultsTitle);
-
-        var searchBarResultsTitleTextDiv = document.createElement("div");
-        searchBarResultsTitle.appendChild(searchBarResultsTitleTextDiv);
-
-        var searchBarResultsTitleText = document.createElement("h5");
-        searchBarResultsTitleText.textContent = "Offline Friends";
-        searchBarResultsTitleText.style = "font-weight: bold;";
-        searchBarResultsTitleTextDiv.appendChild(searchBarResultsTitleText);
-
-        for (var i = 0; i < offlineFriends.length / 3; i++) {
-            var friendCardsDiv = document.createElement("div");
-            friendCardsDiv.className = "search-bar-results-cards";
-            document.getElementsByClassName("search-bar-results")[0].appendChild(friendCardsDiv);
-
-            for (var j = 0; j < 3; j++) {
-                if (!offlineFriends[i * 3 + j]) break;
-
-                var friendCard = document.createElement("div");
+        for (var j = 0; j < offlineFriends.length; j++) {
+            var searchBarResults = document.getElementsByClassName("search-bar-results")[0]
+            var friendCard = document.createElement("div");
                 friendCard.className = "search-bar-results-card";
-                friendCardsDiv.appendChild(friendCard);
+                searchBarResults.appendChild(friendCard);
 
                 var friendCardImg = document.createElement("img"); 
                 friendCardImg.className = "searchPfp";
                 friendCardImg.src = `https://media.valorant-api.com/playercards/9fb348bc-41a0-91ad-8a3e-818035c4e561/smallart.png`;
                 friendCard.appendChild(friendCardImg);
+
+                var friendCardActivity = document.createElement("img"); 
+                friendCardActivity.className = "statusIcon";
+                friendCardActivity.src = `https://cdn.discordapp.com/attachments/808062997180317767/1002731345707016242/offline.png`;
+                friendCard.appendChild(friendCardActivity);
 
                 var friendBannerContainer = document.createElement("div");
                 friendBannerContainer.className = "searchBannerCont";
@@ -115,13 +103,14 @@ fetch("http://127.0.0.1:/friends")
 
                 var friendSearchInfo = document.createElement("div");
                 friendSearchInfo.className = "searchInfo2";
+                friendSearchInfo.id = "friend-search-info";
                 friendCard.appendChild(friendSearchInfo);
 
                 var friendNameText = document.createElement("h1");
                 friendNameText.className = "themeName-large5 textOverflow";
-                friendNameText.textContent = offlineFriends[i * 3 + j].game_name;
+                friendNameText.id = "friend-name";
+                friendNameText.textContent = offlineFriends[j].game_name;
                 friendSearchInfo.appendChild(friendNameText);
-            }
         }
         
-    }).catch((err) => console.log(err));
+}).catch((err) => console.log(err));
