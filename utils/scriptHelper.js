7266cf08defda851c47e9442e9aa2500b947d89a
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2022-12-11 17:27:03 +0000

    little more work

--- utils/scriptHelper.js
+++ utils/scriptHelper.js
@@ -15,31 +15,33 @@ const path = require('path');
 const vulxScriptsPathProd = `${homedir()}/AppData/Local/Vulx/scripts/`;
 const vulxScriptsPathDev = `${process.cwd()}/scripts/`;
 
+const vulxScriptsDirectory = process.pkg ? vulxScriptsPathProd : vulxScriptsPathDev;
+
 class Script {
 	constructor() { }
 
 	async loadScript (scriptFolder, scriptName) {
 		try {
-			const props = require(process.pkg ? `${vulxScriptsPathProd}/${scriptFolder}/${scriptName}` : `${vulxScriptsPathDev}/${scriptFolder}/${scriptName}`);
-            if (props.config === true) {
-                // const hash = encrypt.computeScriptSHA512(__dirname + `/../scripts/${scriptFolder}`);
-                // const isValid = encrypt.checkScriptIntegrity(__dirname + `/../scripts/${scriptFolder}`, hash);
-                // if(!isValid) {
-                //     Logger.error(`Unable to load script ${scriptFolder}/${scriptName}: File has been modified or is unsafe.`)
-                //     return;
-                // } 
-                props.execute();
-				Logger.info(`
-                    \n Loaded Script: ${scriptName} in folder ${scriptFolder} 
-                    \n Name: ${props.cfg.name} 
-                    \n Description: ${props.cfg.description} 
-                    \n Author: ${props.cfg.author}
-                    \n Url: ${props.cfg.url}
-                    \n Version: ${props.cfg.version}
-                `);
-				await SystemMessageHelper.sendSystemMessage(`♡ Loaded ${scriptFolder}/${scriptName}`);
-			}
-			return false;
+			const props = require(`${vulxScriptsDirectory}/${scriptFolder}/${scriptName}`);
+            if (props.config.enabled === false || !props.config.enabled) return false;
+
+			// const hash = encrypt.computeScriptSHA512(__dirname + `/../scripts/${scriptFolder}`);
+			// const isValid = encrypt.checkScriptIntegrity(__dirname + `/../scripts/${scriptFolder}`, hash);
+			// if(!isValid) {
+			//     Logger.error(`Unable to load script ${scriptFolder}/${scriptName}: File has been modified or is unsafe.`)
+			//     return;
+			// } 
+			props.execute();
+			Logger.info(`
+				\n Loaded Script: ${scriptName} in folder ${scriptFolder} 
+				\n Name: ${props.cfg.name} 
+				\n Description: ${props.cfg.description} 
+				\n Author: ${props.cfg.author}
+				\n Url: ${props.cfg.url}
+				\n Version: ${props.cfg.version}
+			`);
+
+			await SystemMessageHelper.sendSystemMessage(`♡ Loaded ${scriptFolder}/${scriptName}`);
 		}
 		catch (e) {
 			Logger.error(`Unable to load script ${scriptName}: ${e}`);
@@ -48,21 +50,21 @@ class Script {
 
     async load() {
         try {
-            await readdir('./scripts/');
+            await readdir(vulxScriptsDirectory);
         } catch (e) {
             Logger.error(`Scripts folder is missing missing. Attemping to create folder. \n${e}`);
             try {
                 Logger.debug(`Creating scripts folder.`);
-                await fs.mkdirSync(path.join(process.cwd(), './scripts/'));
+                await fs.mkdirSync(vulxScriptsDirectory);
             } catch (e) {
                 Logger.error(`Unable to create scripts folder. \n${e}`);
             }
         }
 
-        const scriptFolders = await readdir(path.join(process.cwd(), './scripts/'));
+        const scriptFolders = await readdir(vulxScriptsDirectory);
         Logger.info(`Loading a total of ${scriptFolders.length} scripts.`);
         scriptFolders.forEach(async (f) => {
-            const scriptFiles = await readdir(path.join(process.cwd(), `./scripts/${f}/`));
+            const scriptFiles = await readdir(`${vulxScriptsDirectory}/${f}/`);
             scriptFiles.forEach(async (j) => {
                 if (!j.endsWith('.js')) return;
                 const response = await this.loadScript(f, j);
